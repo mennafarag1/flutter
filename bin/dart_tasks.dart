4c1fa98220c0c4aedb13d0072e51bc434998c619
menna farag <menna.a.farag2000@gmail.com>
2023-07-04 14:29:19 +0300

    task

--- bin/dart_tasks.dart
+++ bin/dart_tasks.dart
@@ -1,5 +1,5 @@
 import 'package:dart_tasks/dart_tasks.dart' as dart_tasks;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_tasks.calculate()}!');
+  print('Hello menna: ${dart_tasks.calculate()}!');
 }
